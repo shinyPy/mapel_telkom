b2266f7975eadbe44cbd59d3a62274c882b788f4
Aedezz <wafielegend2017@gmail.com>
2024-08-19 11:03:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::middleware('JWTAuthentication')->group(function () {
-    
-
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
